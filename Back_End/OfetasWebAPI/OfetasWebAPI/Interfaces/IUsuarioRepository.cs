c42e13fdf113c4878807210b4a91ecfaa2892471
Lucas <lucask.silvamendes@gmail.com>
2022-02-25 15:16:20 -0300

    api iniciada

--- Back_End/OfetasWebAPI/OfetasWebAPI/Interfaces/IUsuarioRepository.cs
+++ Back_End/OfetasWebAPI/OfetasWebAPI/Interfaces/IUsuarioRepository.cs
@@ -1,10 +1,10 @@
-﻿using Patrimonio.Domains;
+﻿using OfetasWebAPI.Domains;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Patrimonio.Interfaces
+namespace OfetasWebAPI.Interfaces
 {
     public interface IUsuarioRepository
     {
--- Back_End/OfetasWebAPI/OfetasWebAPI/ViewModels/LoginViewModel.cs
+++ Back_End/OfetasWebAPI/OfetasWebAPI/ViewModels/LoginViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Patrimonio.ViewModels
+namespace OfetasWebAPI.ViewModels
 {
     public class LoginViewModel
     {
