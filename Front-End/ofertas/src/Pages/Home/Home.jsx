aef96eddd0748615f687bfb68031fd87d81a0c0a
Matheus Martins Nascimento <43866300824@info.scs>
2022-03-04 12:52:13 -0300

    pull senai

--- Front-End/ofertas/src/Pages/Home/Home.jsx
+++ Front-End/ofertas/src/Pages/Home/Home.jsx
@@ -2,14 +2,13 @@ import { Component } from 'react';
 import { useState, useEffect } from 'react';
 import axios, { Axios } from 'axios';
 import { parseJwt, usuarioAutenticado } from '../../services/auth';
-import { Link } from 'react-router-dom';
+import { NavLink, Link, BrowserRouter, useHistory } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { useHistory } from 'react-router-dom';
 
 export default function Home() {
     const [listaProdutos, setListaProdutos] = useState([]);
     const [reservando, setReservando] = useState(false);
-
+    let navigate = useHistory();
     function listarProdutos() {
         axios('https://6220fb53afd560ea69a3da5b.mockapi.io/Ofertas')
             .then(resposta => {
@@ -35,22 +34,22 @@ export default function Home() {
                 })
         } else {
             console.log('nao e consumidor, sem autorizacao para reservar');
-            window.history.
+            navigate('/Login');
         }
     }
 
-    useEffect(listarProdutos, []);
+        useEffect(listarProdutos, []);
 
-    return (
-        <div>
-            {listaProdutos.map((produto) => {
-                return (
-                    <div key={produto.idProduto}>
-                        <h1>Botao reservar</h1>
-                        {reservando ? null : <button onClick={() => reservarProduto(produto)}></button>}
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+        return (
+            <div>
+                {listaProdutos.map((produto) => {
+                    return (
+                        <div key={produto.idProduto}>
+                            <h1>Botao reservar</h1>
+                            {reservando ? null : <button onClick={() => reservarProduto(produto)}></button>}
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
